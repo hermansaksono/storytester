ac66446eb0b57226d6dee02ed83ab079eb321a77
Herman Saksono <hsaksono@ccs.neu.edu>
2020-01-06 16:27:47 -0500

    Update the story JSONs

--- src/app/story-contents/story_list.ts
+++ src/app/story-contents/story_list.ts
@@ -2,7 +2,7 @@ export const STORIES: string[] = [
   'assets/eli_CP.json',
   'assets/unicorn_CP.json',
   'assets/cobby_CP.json',
-  'assets/boomerang_P.json',
+  'assets/boomerang_CP.json',
   'assets/sloth_CP.json',
   'assets/leon_CP.json',
   'assets/flamingo_CP.json',
